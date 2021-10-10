ad1b31c4baa92e5fa57256f87cb5ec690d9e2140
Vlad <75949946+savchinvlad@users.noreply.github.com>
2021-10-10 20:31:10 +0300

    finished layout

--- src/App.js
+++ src/App.js
@@ -93,13 +93,93 @@ function App() {
       <div className="slogan__container">
         <div className="slogan">
           <p>
-            {' '}
             We will carry out a <span className="text__decoration _orange">turnkey</span> move.
             Let's take the <span className="text__decoration _orange"> load</span> off your
             <span className="text__decoration _orange"> shoulders</span>.
           </p>
         </div>
       </div>
+      <div className="possibilities">
+        <div className="assembly__container">
+          <div className="assembly__iamge"></div>
+          <div className="possibilities__content ">
+            <div className="possibilities__content-text"> Assembly</div>
+          </div>
+        </div>
+        <div className="shipping__container">
+          <div className="possibilities__content">
+            <div className="possibilities__content-text">Shipping</div>
+          </div>
+          <div className="shipping__iamge"></div>
+        </div>
+        <div className="unpacking__container">
+          <div className="unpacking__image"></div>
+          <div className="possibilities__content">
+            <div className="possibilities__content-text">Unpacking</div>
+          </div>
+        </div>
+      </div>
+      <div className="benefits__container">
+        <div className="slogan">
+          <p>
+            Four <span className="text__decoration _orange">benefits</span> that will save you a lot
+            of
+            <span className="text__decoration _orange"> valuable</span> time.
+          </p>
+        </div>
+      </div>
+      <div className="benefits__content-container">
+        <div className="benefits__body">
+          <ul className="benefits__list">
+            <li className="benefits__number">01</li>
+            <li className="benefits__number">02</li>
+            <li className="benefits__number">03</li>
+            <li className="benefits__number">04</li>
+          </ul>
+          <ul className="benefits__props-list">
+            <li className="benefits__prop">Kein Suchen nach Baustoffen .</li>
+            <li className="benefits__prop">Keine Zeit für Fahrten verschwenden</li>
+            <li className="benefits__prop">Kein Transportfahrzeug nötig</li>
+            <li className="benefits__prop">
+              Keine komplizierten Bestellungen über Telefon, Mail oder Fax
+            </li>
+          </ul>
+          <div className="benefits__image"></div>
+        </div>
+      </div>
+      <div className="feedback">
+        <div className="feedback__logo">
+          <span className="_semi-black"> Sofortbau</span> is coming
+          <span className="text__decoration _orange"> soon</span>.
+        </div>
+        <div className="feedback__content">Leave your email address to be the first to know.</div>
+        <div className="feedback__mail _mail-text">E-Mail-Adresse *</div>
+        <div className="feedback__button _button">
+          <div className="footer__button-content _button-content"> Absenden</div>
+        </div>
+        <span className="feedback__line"></span>
+      </div>
+      <footer className="footer">
+        <div className="footer__content">
+          <div className="footer__question">Willst du Fahrer werden?</div>
+          <div className="footer__question-content">
+            Bitte kontaktieren Sie uns für eine offene Stelle
+          </div>
+
+          <div className="footer__feedback-container">
+            <div className="footer__mail _mail-small">E-Mail-Adresse *</div>
+            <span className="footer__line-mail"></span>
+            <div className="footer__name _mail-small">vollständiger Name*</div>
+            <div className="footer__phone _mail-small">Telefon</div>
+            <span className="footer__line-name"></span>
+            <span className="footer__line-phone"></span>
+            <div className="footer__button _button">
+              <div className="footer__button-content _button-content">Absenden</div>
+            </div>
+          </div>
+        </div>
+        <div className="footer__image"></div>
+      </footer>
     </div>
   );
 }
